92005906336bd7096966d82245b2d49f8675bce2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-03-27 15:15:17 +0900

    fixed test case

--- aspectj-post-compile-weaving/src/test/java/com/example/service/HelloServiceTest.java
+++ aspectj-post-compile-weaving/src/test/java/com/example/service/HelloServiceTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class HelloServiceTest {
     @Test
     public void hi() throws Exception {
-        assertEquals(0, MyAOP.beforeCount.get());
+        MyAOP.beforeCount.set(0);
         HelloService helloService = new HelloService();
         assertEquals(helloService.hi(), "hi");
         assertEquals(1, MyAOP.beforeCount.get());
--- aspectj-post-compile-weaving/src/test/java/com/example/service/TestTestest.java
+++ aspectj-post-compile-weaving/src/test/java/com/example/service/TestTestest.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.aop.MyAOP;
 import com.example.aop.annotation.Hello;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,8 +10,8 @@ import static org.junit.Assert.assertEquals;
 public class TestTestest {
     @Test
     public void hello() {
+        MyAOP.beforeCount.set(0);
         TestTest testTest = new TestTest();
-        assertEquals(0, MyAOP.beforeCount.get());
         testTest.hello();
         assertEquals(1, MyAOP.beforeCount.get());
     }
