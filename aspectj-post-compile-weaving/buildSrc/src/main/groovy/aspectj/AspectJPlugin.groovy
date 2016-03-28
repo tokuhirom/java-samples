81c02b8942efa354005766c8d5dec00c407cbff4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-03-28 10:13:11 +0900

    use ant tasks

--- aspectj-post-compile-weaving/buildSrc/src/main/groovy/aspectj/AspectJPlugin.groovy
+++ aspectj-post-compile-weaving/buildSrc/src/main/groovy/aspectj/AspectJPlugin.groovy
@@ -1,24 +1,16 @@
 package aspectj
 
-import org.aspectj.bridge.IMessage
-import org.aspectj.bridge.MessageHandler
-import org.aspectj.tools.ajc.Main
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.FileCollection
 import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskAction
-
-import java.nio.file.Path
 
 // taken from spring security.
 // https://github.com/spring-projects/spring-security/blob/master/buildSrc/src/main/groovy/aspectj/AspectJPlugin.groovy
-/**
- *
- * @author Luke Taylor
- */
 class AspectJPlugin implements Plugin<Project> {
 
     void apply(Project project) {
@@ -41,23 +33,21 @@ class AspectJPlugin implements Plugin<Project> {
             project.tasks.create(name: 'compileAspect', overwrite: true, description: 'Compiles AspectJ Source', type: Ajc) {
                 dependsOn project.processResources, project.compileJava
 
+                sourceSet = project.sourceSets.main
+
                 tmpDir = "${project.buildDir}/aspect/"
-                inpath = project.sourceSets.main.output.classesDir.toPath()
-                aspectpath = project.sourceSets.main.output.classesDir
-                classpath = project.sourceSets.main.compileClasspath.files.grep({ it.exists() }).join(":")
-                dstDir = project.sourceSets.main.output.classesDir.toPath()
+                aspectpath = project.files(project.sourceSets.main.output.classesDir)
             }
             project.tasks.classes.dependsOn project.tasks.compileAspect
 
             project.tasks.create(name: 'compileTestAspect', overwrite: true, description: 'Compiles AspectJ Test Source', type: Ajc) {
                 dependsOn project.processTestResources, project.compileTestJava
 
+                sourceSet = project.sourceSets.test
+
                 tmpDir = "${project.buildDir}/test-aspect/"
-                inpath = project.sourceSets.test.output.classesDir.toPath()
-                aspectpath = [project.sourceSets.main.output.classesDir,
-                        project.sourceSets.test.output.classesDir].join(":")
-                classpath = project.sourceSets.test.compileClasspath.files.grep({ it.exists() }).join(":")
-                dstDir = project.sourceSets.test.output.classesDir.toPath()
+                aspectpath = project.files(project.sourceSets.main.output.classesDir,
+                        project.sourceSets.test.output.classesDir)
             }
             project.tasks.testClasses.dependsOn project.tasks.compileTestAspect
         }
@@ -66,13 +56,11 @@ class AspectJPlugin implements Plugin<Project> {
 
 class Ajc extends DefaultTask {
     String tmpDir
-    Path dstDir
-    Path inpath
-    String aspectpath
-    String classpath
+    FileCollection aspectpath
+    SourceSet sourceSet
 
     Ajc() {
-        // logging.captureStandardOutput(LogLevel.INFO)
+        logging.captureStandardOutput(LogLevel.INFO)
     }
 
     //http://www.eclipse.org/aspectj/doc/released/devguide/ajc-ref.html
@@ -84,43 +72,20 @@ class Ajc extends DefaultTask {
         logger.info("=" * 30)
         logger.info("=" * 30)
         logger.info("Running ajc ...")
-        println("inpath: $inpath")
-        println("aspectpath: ${aspectpath}")
-        println("d: $tmpDir")
-        println("classpath: $classpath")
 
-        String[] args = [
-                "-inpath", inpath,
-                "-aspectpath", aspectpath,
-                "-showWeaveInfo",
-                "-1.8",
-                "-d", tmpDir,
-                "-classpath", classpath
-        ];
-        println(project.sourceSets.main.compileClasspath)
-
-        MessageHandler handler = new MessageHandler(false);
-        println("args: $args")
-        new Main().run(args as String[], handler);
-        for (IMessage message : handler.getMessages(null, true)) {
-            switch (message.getKind()) {
-                case IMessage.ABORT:
-                case IMessage.ERROR:
-                case IMessage.FAIL:
-                    logger.error message.message, message.thrown
-                    break;
-                case IMessage.WARNING:
-                    logger.warn message.message, message.thrown
-                    break;
-                case IMessage.INFO:
-                    logger.info message.message, message.thrown
-                    break;
-                case IMessage.DEBUG:
-                    logger.debug message.message, message.thrown
-                    break;
-            }
+        ant.taskdef(resource: "org/aspectj/tools/ant/taskdefs/aspectjTaskdefs.properties", classpath: project.configurations.ajtools.asPath)
+        ant.iajc(
+                classpath: sourceSet.compileClasspath.asPath,
+                fork: 'true',
+                inpath: sourceSet.output.classesDir.toPath(),
+                destDir: tmpDir,
+                source: project.convention.plugins.java.sourceCompatibility,
+                target: project.convention.plugins.java.targetCompatibility,
+                // aspectPath: aspectPath.asPath,
+                aspectPath: aspectpath.asPath,
+                showWeaveInfo: 'true') {
         }
 
-        ant.move(file: tmpDir, tofile: dstDir)
+        ant.move(file: tmpDir, tofile: sourceSet.output.classesDir)
     }
 }
