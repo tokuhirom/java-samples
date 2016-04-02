27ffb1825f80960b84f40398c91cca95ae2af595
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-02 09:31:29 +0900

    sample

---
+++ spring-boot-mybatis/src/main/java/com/example/domain/Blog.java
@@ -0,0 +1,9 @@
+package com.example.domain;
+
+import lombok.Data;
+
+@Data
+public class Blog {
+    private long id;
+    private String title;
+}
---
+++ spring-boot-mybatis/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into blog (title) values ('hoge');
+insert into blog (title) values ('fuga');
